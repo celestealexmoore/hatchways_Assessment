8fd0f1d7f63324da0a09abedf7fb2543b0a0f13f
Celeste Moore <celestealexmoore@gmail.com>
2022-04-08 18:22:14 -0500

    appended pic, name, email, company and skill to document

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 let requestURL = 'https://api.hatchways.io/assessment/students';
 
+
 /* 
 When trying to get data from a third-party API, 
 
@@ -29,7 +30,48 @@ function getAPI() {
             console.log(data)
 
             for(let i = 0; i < data['students'].length; i++){
-                console.log(data['students'][i]['firstName']) 
+
+                //picture
+                let photo = document.createElement('img');
+                photo.setAttribute (
+                    "src",
+                    data['students'][i]['pic']
+                );
+                //name
+                let fullName = document.createElement('h3');
+                fullName.textContent = data['students'][i]['firstName'] + " " + data['students'][i]['lastName'];
+                //email
+                let email = document.createElement('p');
+                email.textContent = data['students'][i]['email'];
+                //company
+                let company = document.createElement('p');
+                company.textContent = data['students'][i]['company'];
+                //skill
+                let skill = document.createElement('p');
+                skill.textContent = data['students'][i]['skill'];
+
+
+                //average
+
+                /* 
+                function findAverage(grades) {
+                let total = 0;
+                for(let i = 0; i < grades.length; i++) {
+                total += grades;
+                }
+                let average = total / grades.length;
+                console.log(average);
+                }
+
+                findAverage(data['students'][i]['grades'])
+                */
+                
+                body.appendChild(photo)
+                body.appendChild(fullName)
+                body.appendChild(email)
+                body.appendChild(company)
+                body.appendChild(skill)
+    
             }
 
             console.log(data['students'].length)
