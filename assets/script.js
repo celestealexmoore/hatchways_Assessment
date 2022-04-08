6e1a3296552861e3b65c8a1c37fc6bb62e9f99b1
Celeste Moore <celestealexmoore@gmail.com>
2022-04-08 18:24:02 -0500

    cleaned up code

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,9 @@
 let requestURL = 'https://api.hatchways.io/assessment/students';
 
-
-/* 
-When trying to get data from a third-party API, 
-
-1. Make sure that data exists. (You can do so using curl in the terminal.)
-2. JSONify that data, (turn it into a javascript object so that you can read it).
-3. Console.log the data and use the data.
-
-4. Follow the data types in the response.
-5. Target specific information
-6. Traverse (loop) over the array.
-
-This is a great example of boiler plate code.
-
-
-Curl is back-facing. It is not useful to us when trying to display information to the user via the browser. It is only useful to us
-who interact with the terminal.
-*/
-
 function getAPI() {
 
-    fetch(requestURL) // asynchronous call to fetch the resources at our requested url
-        .then(function(response) { // .then is a callback function itself
+    fetch(requestURL)
+        .then(function(response) {
             return response.json();
         })
         .then(function(data) {
@@ -49,10 +30,7 @@ function getAPI() {
                 //skill
                 let skill = document.createElement('p');
                 skill.textContent = data['students'][i]['skill'];
-
-
                 //average
-
                 /* 
                 function findAverage(grades) {
                 let total = 0;
@@ -62,7 +40,6 @@ function getAPI() {
                 let average = total / grades.length;
                 console.log(average);
                 }
-
                 findAverage(data['students'][i]['grades'])
                 */
                 
@@ -71,12 +48,7 @@ function getAPI() {
                 body.appendChild(email)
                 body.appendChild(company)
                 body.appendChild(skill)
-    
             }
-
-            console.log(data['students'].length)
-           // console.log(data['students'][3]['firstName']) // It's formatted this way due to the order of the data types in the JSON'd response.
-            //           ^       ^     ^     ^ 
         })
 }
 
