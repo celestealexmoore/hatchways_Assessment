815c702c18b2ff02411bfe6f3d091360bec62e82
Celeste Moore <celestealexmoore@gmail.com>
2022-04-09 18:26:10 -0500

    updated parent-child relationships

--- assets/script.js
+++ assets/script.js
@@ -2,34 +2,61 @@ let requestURL = 'https://api.hatchways.io/assessment/students';
 
 function getAPI() {
 
+    // performing fetch request and translating into json object:
     fetch(requestURL)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
 
+            // to view data response from fetch:
             console.log(data)
 
-            for(let i = 0; i < data['students'].length; i++){
+            for(let i = 0; i < data['students'].length; i++) {
+   
+                // for flexbox div structure:
+                let parentDiv = document.createElement('div');
+                parentDiv.setAttribute('id', 'parentDiv');
+                let photoDiv = document.createElement('div');
+                photoDiv.setAttribute('id', 'photoDiv');
+                let userInfoDiv = document.createElement('div');
+                userInfoDiv.setAttribute('id', 'userInfoDiv');
+                let nameDiv = document.createElement('div');
+                nameDiv.setAttribute('id', 'nameDiv');
+                let detailsDiv = document.createElement('div');
+                detailsDiv.setAttribute('id', 'detailsDiv');
 
-                //picture
+                // appending children divs to parent divs:
+                body.appendChild(parentDiv);
+                parentDiv.appendChild(photoDiv);
+                parentDiv.appendChild(userInfoDiv);
+                userInfoDiv.appendChild(nameDiv);
+                userInfoDiv.appendChild(detailsDiv);
+
+                // picture element:
                 let photo = document.createElement('img');
+                photo.setAttribute('class', 'photo');
                 photo.setAttribute (
                     "src",
                     data['students'][i]['pic']
                 );
-                //name
+                // name element:
                 let fullName = document.createElement('h3');
-                fullName.textContent = data['students'][i]['firstName'] + " " + data['students'][i]['lastName'];
-                //email
+                fullName.setAttribute('class', 'fullName');
+                fullName.textContent = data['students'][i]['firstName'].toUpperCase() + " " + data['students'][i]['lastName'].toUpperCase();
+                // email element:
                 let email = document.createElement('p');
-                email.textContent = data['students'][i]['email'];
-                //company
+                email.setAttribute('class', 'details');
+                email.textContent = "Email: " + data['students'][i]['email'];
+                // company element:
                 let company = document.createElement('p');
-                company.textContent = data['students'][i]['company'];
-                //skill
+                company.setAttribute('class', 'details');
+                company.textContent = "Company: " + data['students'][i]['company'];
+                // skill element:
                 let skill = document.createElement('p');
-                skill.textContent = data['students'][i]['skill'];
+                skill.setAttribute('class', 'details');
+                skill.textContent = "Skill: " + data['students'][i]['skill'];
+
                 //average
                 /* 
                 function findAverage(grades) {
@@ -43,11 +70,12 @@ function getAPI() {
                 findAverage(data['students'][i]['grades'])
                 */
                 
-                body.appendChild(photo)
-                body.appendChild(fullName)
-                body.appendChild(email)
-                body.appendChild(company)
-                body.appendChild(skill)
+                // appending children elements to parent divs:
+                photoDiv.appendChild(photo);
+                nameDiv.appendChild(fullName);
+                detailsDiv.appendChild(email);
+                detailsDiv.appendChild(company);
+                detailsDiv.appendChild(skill);
             }
         })
 }
